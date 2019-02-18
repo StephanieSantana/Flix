54a36b8bb3cebbb02302a67269fa38b4b6709b37
stephaniesantana <stephycsantana@gmail.com>
2019-02-17 20:03:53 -0800

    Part 2 of Flix

---
+++ Flixster/Flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Stephanie Santana on 2/17/19.
+//  Copyright © 2019 Stephanie Santana. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
